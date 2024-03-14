4f847c27da868037051a91aeddbd80a81cbf3a2c
David <davidstudiosco@gmail.com>
2024-03-14 13:40:56 +0100

    Se ha añadido texto

--- app/app.py
+++ app/app.py
@@ -2,5 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1> ¡Hola Mundo desde Flask! ¿Qué tal? </h1>'
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=5000)
