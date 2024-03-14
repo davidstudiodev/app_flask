e3db2a34ea98aa806a1b3e45251e963d070d7555
David <davidstudiosco@gmail.com>
2024-03-14 14:32:19 +0100

    Tenemos pagina dinamica

--- app/app.py
+++ app/app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1> ¡Hola Mundo desde Flask! ¿Qué tal? </h1>'
+    #return '<h1> ¡Hola Mundo desde Flask! ¿Qué tal? </h1>'
+    
+    cursos = ['PHP', 'Python', 'Java', 'C#']
+    
+    data = {
+        'Titulo': 'Index',
+        'Bienvenida': '¡Saludo!',
+        'Cursos': cursos,
+        'Numero_cursos': len(cursos)
+    }
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=65342)
